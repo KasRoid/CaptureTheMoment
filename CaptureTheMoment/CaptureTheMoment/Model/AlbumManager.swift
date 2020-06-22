e4f0c02715b319e9b1861c1824aeff74b5931cc9
KasRoid <KasRoid@gmail.com>
2020-06-22 10:14:03 +0900

    File: 앨범 이미지 정보를 관리할 Album Manager 생성

---
+++ CaptureTheMoment/CaptureTheMoment/Model/AlbumManager.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumManager.swift
+//  CaptureTheMoment
+//
+//  Created by Doyoung Song on 6/22/20.
+//  Copyright © 2020 Doyoung Song. All rights reserved.
+//
+
+import UIKit
+
+struct AlbumManager {
+    var image: UIImage?
+    var imageName: String?
+    var imageTag: String?
+}
