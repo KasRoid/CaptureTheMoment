a6c3b647f3c32e0d11bb3ec0d45e3ff8165b0e78
KasRoid <KasRoid@gmail.com>
2020-06-22 16:57:10 +0900

    UI: Comment Title Label 추가, AutoLayout 재설정

--- CaptureTheMoment/CaptureTheMoment/Controllers/DetailViewController.swift
+++ CaptureTheMoment/CaptureTheMoment/Controllers/DetailViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 final class DetailViewController: UIViewController {
 
     // MARK: - Properties
-    lazy var imageViewer: UIImageView = {
+    private lazy var imageViewer: UIImageView = {
         var imageView = UIImageView()
         imageView.image = UIImage(named: "")
         imageView.backgroundColor = .white
@@ -20,10 +20,21 @@ final class DetailViewController: UIViewController {
         return imageView
     }()
     
+    private lazy var commentTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Comment"
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textColor = .white
+        label.layer.borderWidth = 2.0
+        label.layer.borderColor = UIColor.clear.cgColor
+        label.backgroundColor = .clear
+        return label
+    }()
+    
     private lazy var commentLabel: UILabel = {
         let label = UILabel()
         label.text = "여기에 이런식으로 사용자가 작성한 코멘트가 불러와 보여집니다"
-        label.font = UIFont.systemFont(ofSize: 25)
+        label.font = UIFont.systemFont(ofSize: 22)
         label.textColor = .white
         label.layer.borderWidth = 2.0
         label.layer.borderColor = UIColor.clear.cgColor
@@ -61,7 +72,7 @@ final class DetailViewController: UIViewController {
         navigationItem.leftBarButtonItems = [returnBtn]
         navigationItem.rightBarButtonItems = [editBtn]
         
-        [imageViewer, commentLabel].forEach {
+        [imageViewer, commentTitleLabel, commentLabel].forEach {
             view.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
@@ -73,9 +84,14 @@ final class DetailViewController: UIViewController {
             imageViewer.widthAnchor.constraint(equalToConstant: view.bounds.width - spacing * 2),
             imageViewer.heightAnchor.constraint(equalToConstant: view.bounds.height * 3 / 5),
             
+            commentTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            commentTitleLabel.topAnchor.constraint(equalTo: imageViewer.bottomAnchor, constant: spacing * 2),
+            commentTitleLabel.leadingAnchor.constraint(equalTo: imageViewer.leadingAnchor, constant: 0),
+            commentTitleLabel.heightAnchor.constraint(equalToConstant: 30),
+            
             commentLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
-            commentLabel.topAnchor.constraint(equalTo: imageViewer.bottomAnchor, constant: spacing * 3),
-            commentLabel.widthAnchor.constraint(equalToConstant: view.bounds.width - spacing * 2),
+            commentLabel.topAnchor.constraint(equalTo: commentTitleLabel.bottomAnchor, constant: spacing / 3),
+            commentLabel.leadingAnchor.constraint(equalTo: imageViewer.leadingAnchor, constant: 0),
             commentLabel.heightAnchor.constraint(equalToConstant: 30),
             ].forEach { $0.isActive = true }
     }
